c5bc7b85ccd6431e8a2c086741be3d8f8dcea8bc
SinaSystem <sinasystem47@gmail.com>
2020-06-05 13:20:59 +0430

    Initial commit

---
+++ app/src/main/java/com/example/android/roomwordssample/WordRepository.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.roomwordssample
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+
+
+class WordRepository(private val wordDao: WordDao) {
+
+
+    //private WordDao mWordDao;
+   // private LiveData<List<Word>> mAllWords;
+
+    //this code is equivalent two line code above in java
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    //this code is equivalent  two series line code below in kotlin
+
+
+    /*   @NotNull
+       public final LiveData getAllWords() {
+           return this.allWords;
+       }
+   */
+
+
+    //or
+   /* var allWords :LiveData<List<Word>>? = null
+    fun getAllWords(): LiveData<List<Word>>? {
+        allWords = wordDao.getAlphabetizedWords()
+        return allwords
+    }*/
+
+
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
